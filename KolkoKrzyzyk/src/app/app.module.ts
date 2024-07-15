5d1ff6348d9c96c6276deb3d9780534aa44f2709
DarekP <darek.piechota@gmail.com>
2024-07-15 07:56:03 +0200

    First look

--- KolkoKrzyzyk/src/app/app.module.ts
+++ KolkoKrzyzyk/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Level1Component } from './level1/level1.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Level1Component],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
